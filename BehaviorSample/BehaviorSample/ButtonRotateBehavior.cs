c6f5056cfee464dc13ffd8939d1f4cf339eb2556
billchungiii <billchungiii@gmail.com>
2016-12-19 17:44:19 +0800

    加入專案檔案。

---
+++ BehaviorSample/BehaviorSample/ButtonRotateBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BehaviorSample
+{
+    public  class ButtonRotateBehavior : Behavior<Button>
+    {  
+        protected override void OnAttachedTo(Button bindable)
+        {
+            bindable.Clicked += Bindable_Clicked; 
+            base.OnAttachedTo(bindable);
+        }
+
+        async private void Bindable_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            await button.RotateTo(45, 500, Easing.BounceOut);
+            await button.RotateTo(0, 500, Easing.BounceOut);
+        }
+
+        protected override void OnDetachingFrom(Button bindable)
+        {
+            bindable.Clicked -= Bindable_Clicked;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
